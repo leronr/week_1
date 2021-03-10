511ecc146f84f01623b7c71a5822094b1d819080
leronr <80352521+leronr@users.noreply.github.com>
2021-03-10 12:10:15 -0500

    python example

---
+++ day3/pythonExample.py
@@ -0,0 +1,27 @@
+# assignment in python
+# number = 1
+# number = 1
+greeting = "Hello Digitalcrafts Family"
+# Float === 4.5, 4.55565656, 4.
+classSize = 23
+
+floatExamples = 2.323
+
+sumOfNumbers = 1 + 1
+
+# print(f'{number}')
+somethingToCallit = ["students", 23, True]
+# somethingToCallit = {"helloPythoworld": 23, "we are students"}
+# [1, 2, 3]
+# ["strings", "strings2", "blahblahblah"]
+print("Hello World")
+print("My name is Joe and I teach")
+
+variableName= True
+
+
+
+# [False, True, True]
+# ["list", ["list"]]
+# Dictionary
+#
